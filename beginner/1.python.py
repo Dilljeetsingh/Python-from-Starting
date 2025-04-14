4bd81099757a965351d5a4e1123ed2c4a60aad19
Diljeet Singh <135757591+Dilljeetsingh@users.noreply.github.com>
2025-04-14 13:32:20 -0400

    beginner step no 1 and 2 half
    it here I done how to print in console and start learning about variable. where i know integer.

---
+++ beginner/1.python.py
@@ -0,0 +1,3 @@
+# This is my first python program using print only
+print('hello my friend')
+print('how are you doing')
---
+++ beginner/2.Variable.py
@@ -0,0 +1,11 @@
+# variable = A container for a value (string, integer, float, boolean)
+#            A varibale behaves as if it was the vaue it contains
+
+first_name = "deepak"
+food = "biryani"
+email = 'deepak@pyhton.com'
+
+
+print(f"hello {first_name}")
+print(f"you like {food}")
+print(f"your email is: {email}")
