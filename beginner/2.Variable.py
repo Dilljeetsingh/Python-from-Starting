3ae5d42d49b716fd053ad612b93b4864f7efe55f
Diljeet Singh <135757591+Dilljeetsingh@users.noreply.github.com>
2025-04-14 15:22:39 -0400

    now i have done Foat which is similar but with ( . ) dot. and done boolean which is important and learn about if and else and what means true or false

--- beginner/2.Variable.py
+++ beginner/2.Variable.py
@@ -1,11 +1,28 @@
 # variable = A container for a value (string, integer, float, boolean)
 #            A varibale behaves as if it was the vaue it contains
 
+# Strings
 first_name = "deepak"
 food = "biryani"
 email = 'deepak@pyhton.com'
 
+# Integers
+age = 25
+quantity = 3
+num_of_students = 30
 
-print(f"hello {first_name}")
-print(f"you like {food}")
-print(f"your email is: {email}")
+# Float
+price = 10.99
+distance = 5.5
+
+# Boolean
+
+is_student = True
+for_sale = False
+
+if for_sale:
+    print("you have a sale")
+
+else:
+    print("you haven't a sale")
+
