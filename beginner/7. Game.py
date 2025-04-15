e5eac8220c46946a961ea411f6716a277ccdb0ad
Diljeet Singh <135757591+Dilljeetsingh@users.noreply.github.com>
2025-04-14 21:55:52 -0400

    create a Game called madlibs
    very simple

---
+++ beginner/7. Game.py
@@ -0,0 +1,14 @@
+# Madlibs game
+# word game where you create a story 
+# by filling in blanks with random 
+
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun(person, place, thing): ")
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb ending with ing: ")
+noun2 = input("Enter a noun(person, place, thing): ")
+
+print(f"Today I am killing {adjective1} with my friend.")
+print(f"It was fun but if we visit {noun1}.")
+print(f"{noun2}is good envinmental and {verb1}")
+print(f"I was {adjective2}")
