327749631936b3d32fc0cde5f22c8c0974a350a1
Diljeet Singh <135757591+Dilljeetsingh@users.noreply.github.com>
2025-04-14 22:06:47 -0400

    did arithmetic operators

---
+++ beginner/8. Arithmetic.operators.py
@@ -0,0 +1,28 @@
+# arithmetic operators math functions
+
+friends = 5
+
+# addition
+# friends = friends + 1
+# friends += 1
+
+# subtraction
+# friends = friends - 1
+# friends -= 1
+
+# multiple
+# friends = friends * 2
+# friends *= 2
+
+# divide
+# friends = friends / 2
+# friends /= 2
+
+# power of 
+# friends = friends ** 2
+# friends **= 2
+
+#modules
+# remainder = friends % 3   
+
+print(friends)
